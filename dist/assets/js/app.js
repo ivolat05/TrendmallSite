bda3faa83a23f88a0a99b6eae9156f9bee0c2380
andrei_labadaev <andrei-labadaev@mail.ru>
2021-12-17 12:28:22 +0300

    uppdate 12.28

---
+++ dist/assets/js/app.js
@@ -0,0 +1,94 @@
+// select
+let select = function () {
+    let selectHeader = document.querySelectorAll('.select__header');
+
+    let selectItem = document.querySelectorAll('.select__item');
+
+    selectHeader.forEach((item) => {
+        item.addEventListener('click', selectToggle)
+    })
+
+    selectItem.forEach((item) => {
+        item.addEventListener('click', selectChoose)
+    })
+
+    function selectToggle() {
+        this.parentElement.classList.toggle('is-active');
+    }
+
+    function selectChoose() {
+        let text = this.innerText;
+        let select = this.closest('.select');
+        let currentText = select.querySelector(".select__current");
+        currentText.innerText = text;
+        select.classList.remove('is-active');
+    }
+
+
+}
+
+// показывать скрывать пароль
+function showPassword() {
+    let btn = document.querySelectorAll('.cabinet__password-btn');
+    let inputPassword = document.querySelectorAll('.input-password');
+    btn.forEach((item) => {
+        item.addEventListener('click', function () {
+            let passwordId = item.getAttribute('data-password');
+            let showPassword = document.querySelector(passwordId);
+
+            if (showPassword.getAttribute('type') === 'password') {
+                showPassword.type = 'text';
+            } else {
+                showPassword.type = 'password';
+            }
+        })
+    })
+}
+
+// tabs 
+function tabs() {
+    const cabinetBtn = document.querySelectorAll('.cabinet__btn');
+    const cabinetTab = document.querySelectorAll('.cabinet__tab');
+
+    cabinetBtn.forEach((item) => {
+        item.addEventListener('click', function () {
+            let tabId = item.getAttribute('data-tab');
+            let currentTab = document.querySelector(tabId);
+
+            cabinetBtn.forEach(function (item) {
+                item.classList.remove('cabinet__btn-acive')
+            })
+
+            cabinetTab.forEach(function (item) {
+                item.classList.remove('cabinet__tab-active')
+            })
+            item.classList.add('cabinet__btn-acive');
+            currentTab.classList.add('cabinet__tab-active');
+        });
+    });
+
+}
+
+// открытие меню изменения данных
+function dataMenuHidden() {
+    let cabinetBtnSave = document.querySelector('.cabinet__btn-save');
+    let cabinetBtnChange = document.querySelector('.cabinet__btn-change');
+    let cabinetDataBox = document.querySelector('.cabinet__data-box ');
+    let cabinetDataInner = document.querySelector('.cabinet__data-inner');
+
+    cabinetBtnChange.addEventListener('click', function () {
+        cabinetBtnChange.classList.add('cabinet__tab-data--hidden');
+        cabinetDataInner.classList.add('cabinet__tab-data--hidden');
+        cabinetDataBox.classList.remove('cabinet__tab-data--hidden');
+    })
+    cabinetBtnSave.addEventListener('click', function () {
+        cabinetBtnChange.classList.remove('cabinet__tab-data--hidden');
+        cabinetDataInner.classList.remove('cabinet__tab-data--hidden');
+        cabinetDataBox.classList.add('cabinet__tab-data--hidden');
+    })
+};
+
+select();
+showPassword();
+tabs();
+dataMenuHidden();
